da939c4da7c04a8689877090b7d44ee336e80848
chicoumbetov <shyngysmaaan@gmail.com>
2022-10-05 22:52:27 +0200

    :lipstick: styles added NewsCards

--- src/components/NewsCards/NewsCards.tsx
+++ src/components/NewsCards/NewsCards.tsx
@@ -1,16 +1,23 @@
 import { Grid, Grow } from "@material-ui/core";
 import NewsCard from "../NewsCard/NewsCard";
+import useStyles from "./styles";
 
 type NewsCardProps = {
   name: string;
 };
 
 const NewsCards = (articles: any) => {
+  const styles = useStyles();
   console.log("articles : ", articles);
 
   return (
     <Grow in>
-      <Grid container alignItems="stretch" spacing={3}>
+      <Grid
+        className={styles.container}
+        container
+        alignItems="stretch"
+        spacing={3}
+      >
         {articles?.articles?.map((article: any, i: number) => (
           <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: "flex" }}>
             <NewsCard key={article.id} article={article} />
