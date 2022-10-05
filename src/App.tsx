b7d249867010fc1c6c799113ebbe98f07780935d
chicoumbetov <shyngysmaaan@gmail.com>
2022-10-05 18:01:57 +0200

    :sparkles: First commit with basic comps

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { useLayoutEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const alanKey =
+  "84be8b666e82b0314c486716c22a85902e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+const App = () => {
+  const [newsArticles, setNewArticles] = useState();
+
+  useLayoutEffect(() => {
+    function updateScreen() {
+      // Make visual updates here.
+      alanBtn({
+        key: alanKey,
+        onCommand: (commandData: any) => {
+          if (commandData?.command === "newHeadlines") {
+            // Call the client code that will react to the received command
+            setNewArticles(commandData?.articles);
+          }
+        },
+      });
+    }
+
+    requestAnimationFrame(updateScreen);
+  }, []);
+
+  return <div>Alan AI Voice assistance application</div>;
+};
 
 export default App;
